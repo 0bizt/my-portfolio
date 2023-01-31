0016d45a36682aa497e6e20aaa6167f77e32be0a
jerry <jerrynwosu007@gmail.com>
2023-01-31 02:59:52 +0100

    update: add avatar

--- portfolio.config.js
+++ portfolio.config.js
@@ -3,8 +3,7 @@ import { IoLogoGithub, IoLogoTwitter } from 'react-icons/io';
 export const data = {
   author: 'Jerry Nwosu',
   title: 'Software Engineer',
-  avatar:
-    'https://media.licdn.com/dms/image/C5603AQEEfwyQYyHXHw/profile-displayphoto-shrink_800_800/0/1613826760366?e=1680134400&v=beta&t=sltVISQB9nr2CCn4VSuxDsUItup-uaT7HRc-olvo7Tc',
+  avatar: '/avatar.jpeg',
   socials: [
     {
       title: 'Twitter',
