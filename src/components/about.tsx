823c41b9c4590f6cf9c75addd282f03abd24a30a
jerry <jerrynwosu007@gmail.com>
2023-01-31 01:15:38 +0100

    update

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -58,51 +58,19 @@ const About: React.FC = () => {
               <span>Software Engineer</span>
             </h2>
           </div>
-          {/* <div>
-            <p className={[styles.desc, styles.mt_32].join(" ")}>
-              Hi, I&apos;m Joshua Nwosu a software engineer (and sometimes UI/UX
-              designer).
-            </p>
-            <p className={[styles.desc, styles.mt_8].join(" ")}>
-              I build open-source{" "}
-              <a
-                href="https://github.com/joshnwosu"
-                target="_blank"
-                rel="noreferrer"
-              >
-                front-end libraries
-              </a>{" "}
-              and also contribute to open-source projects with over 5+ years of
-              experience.
-            </p>
-            <p className={[styles.desc, styles.mt_8].join(" ")}>
-              When I am not occupied with my daily responsibilities, I like to
-              share my knowledge and experiences with others by posting about
-              the things I have learned. I find it to be a fulfilling way to
-              reflect on my growth and help others in the process.
-            </p>
-          </div> */}
           <div>
             <p className={[styles.desc, styles.mt_32].join(' ')}>
-              Hi, I am {data.author}, a {data.title} with over five years of
-              experience in the tech industry. My expertise lies in creating
-              efficient and user-friendly software solutions. I am driven by my
-              passion for programming and constantly seeking new challenges to
-              improve my skills.
+              Hello! I am a software engineer with 5+ years of experience in the
+              field. I specialize in developing high-quality and efficient
+              applications using various programming languages and technologies.
+              With a strong understanding of the software development life cycle
+              and a passion for problem-solving, I am dedicated to creating
+              innovative and user-friendly solutions that meet the needs of
+              clients and end-users.
             </p>
             <p className={[styles.desc, styles.mt_8].join(' ')}>
-              I am also an active member of the open-source community,{' '}
-              <a
-                href="https://github.com/joshnwosu"
-                target="_blank"
-                rel="noreferrer"
-              >
-                building libraries
-              </a>{' '}
-              and contributing to projects. I believe in sharing my knowledge
-              and helping others to grow in the field of technology. I am always
-              striving to create top-notch software that makes a positive
-              impact.
+              As a self-motivated individual, I am constantly seeking new
+              challenges and opportunities to expand my knowledge and skills.
             </p>
           </div>
         </div>
